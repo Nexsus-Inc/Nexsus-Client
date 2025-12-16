d43d22f8c6bab71219b487c9e713f4bbc5e7f030
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:52:42 +0100

    feat: discord ipc

---
+++ src/main/java/de/constt/nexsus_client/client/discordRPC/discordIPCCore.java
@@ -0,0 +1,69 @@
+package de.constt.nexsus_client.client.discordRPC;
+
+import com.google.gson.JsonObject;
+import com.jagrosh.discordipc.IPCClient;
+import com.jagrosh.discordipc.IPCListener;
+import com.jagrosh.discordipc.entities.ActivityType;
+import com.jagrosh.discordipc.entities.Packet;
+import com.jagrosh.discordipc.entities.RichPresence;
+import com.jagrosh.discordipc.entities.StatusDisplayType;
+import com.jagrosh.discordipc.entities.User;
+import com.jagrosh.discordipc.exceptions.NoDiscordClientException;
+import de.constt.nexsus_client.client.config.NexsusConfigData;
+import de.constt.nexsus_client.client.states.AddressState;
+import net.minecraft.client.MinecraftClient;
+
+import java.time.OffsetDateTime;
+
+public final class discordIPCCore {
+
+    private static IPCClient client;
+    private static final long CLIENT_ID = 1450138306141618227L;
+
+    public static void start() throws NoDiscordClientException {
+        client = new IPCClient(CLIENT_ID);
+
+        MinecraftClient mc = MinecraftClient.getInstance();
+
+        String clientVersion = NexsusConfigData.getConfigValue("clientVersion");
+
+
+        var v = net.minecraft.SharedConstants.getGameVersion();
+        String versionId = v.getId();
+
+        assert mc.getServer() != null;
+
+        client.setListener(new IPCListener() {
+            @Override
+            public void onReady(IPCClient client) {
+                RichPresence.Builder builder = new RichPresence.Builder()
+                        .setActivityType(ActivityType.Playing)
+                        .setStatusDisplayType(StatusDisplayType.State)
+                        .setName("Nexsus Client")
+                        .setState("v"+clientVersion)
+                        .setDetails(AddressState.getAddress()+" | "+versionId)
+                        .setLargeImage("logo")
+                        .setStartTimestamp(OffsetDateTime.now().toEpochSecond());
+
+                client.sendRichPresence(builder.build());
+            }
+
+            @Override public void onPacketSent(IPCClient c, Packet p) {}
+            @Override public void onPacketReceived(IPCClient c, Packet p) {}
+            @Override public void onActivityJoin(IPCClient c, String s) {}
+            @Override public void onActivitySpectate(IPCClient c, String s) {}
+            @Override public void onActivityJoinRequest(IPCClient c, String s, User u) {}
+            @Override public void onClose(IPCClient c, JsonObject j) {}
+            @Override public void onDisconnect(IPCClient c, Throwable t) {}
+        });
+
+        client.connect();
+    }
+
+    public static void shutdown() {
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+    }
+}
