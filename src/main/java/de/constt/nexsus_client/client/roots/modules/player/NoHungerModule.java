9df51eff0b1545a0776e7b195a6ef3e046c5d620
constt <175602385+constttdev@users.noreply.github.com>
2025-12-16 20:56:44 +0100

    feat: module info feat: cancel start sprinting packet

--- src/main/java/de/constt/nexsus_client/client/roots/modules/player/NoHungerModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/player/NoHungerModule.java
@@ -1,22 +1,26 @@
-package de.constt.nexsus_client.client.roots.modules;
+package de.constt.nexsus_client.client.roots.modules.player;
 
+import de.constt.nexsus_client.client.Nexsus_clientClient;
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
 import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
+@InfoAnnotation(
+        name = "No Hunger",
+        description = "Reduces the hunger taken by e.g. sprinting (Does not remove hunger)",
+        category = CategoryImplementation.Categories.PLAYER
+)
 public class NoHungerModule extends ModuleImplementation {
-    // In progress
-
     @Override
     public boolean modifyPacket(Packet<?> packet) {
-        return false;
-    }
+        if (!(packet instanceof ClientCommandC2SPacket commandPacket)) return false;
 
-    @Override
-    public void onTick() {
-
+        return commandPacket.getMode() == ClientCommandC2SPacket.Mode.START_SPRINTING;
     }
 }
