43349f8751a76b47e4f4878fb973ee4da8aceb1b
constt <175602385+constttdev@users.noreply.github.com>
2025-12-17 21:02:37 +0100

    feat: modules screen

---
+++ src/main/java/de/constt/nexsus_client/client/roots/gui/ModulesScreen.java
@@ -0,0 +1,49 @@
+package de.constt.nexsus_client.client.roots.gui;
+
+import de.constt.nexsus_client.client.helperFunctions.annotationHelperFunction;
+import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.roots.gui.widgets.BlackButtonWidget;
+import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
+import de.constt.nexsus_client.client.roots.modules.ModuleManager;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.Text;
+
+public class ModulesScreen extends Screen {
+    public ModulesScreen(String title) {
+        super(Text.literal(title));
+    }
+
+    @Override
+    protected void init() {
+        int x = 10;
+        int y = 10;
+        int spacing = 20;
+
+        for (ModuleImplementation module : ModuleManager.getModules()) {
+            this.addDrawableChild(new BlackButtonWidget(x, y, 100, 15, annotationHelperFunction.getName(module.getClass()),button -> {
+                module.toggle();
+                String status = module.getEnabledStatus() ? "on" : "off";
+                String statusColorCoded = status;
+                if(status.equals("on")) {
+                    statusColorCoded = "§aon";
+                } else {
+                    statusColorCoded = "§coff";
+                }
+
+                if (MinecraftClient.getInstance().player != null) {
+                    chatHelperFunction.sendCSMessageNeutral("§8Toggled§r "+annotationHelperFunction.getName(module.getClass()) + " (" + statusColorCoded + ")");
+                }
+            }));
+
+            y += spacing;
+        }
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
+    }
+}
