3e2eb8b12c5e91c42d2a973d3a755c0b98a39f99
constt <175602385+constttdev@users.noreply.github.com>
2025-12-17 21:02:11 +0100

    feat: show toggled modules feat: show fps

--- src/main/java/de/constt/nexsus_client/client/helperFunctions/renderHelperFunction.java
+++ src/main/java/de/constt/nexsus_client/client/helperFunctions/renderHelperFunction.java
@@ -1,10 +1,15 @@
 package de.constt.nexsus_client.client.helperFunctions;
 
 import de.constt.nexsus_client.client.config.NexsusConfigData;
+import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
+import de.constt.nexsus_client.client.roots.modules.ModuleManager;
+import de.constt.nexsus_client.client.roots.modules.render.FPSHudModule;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.math.MathHelper;
+
+import java.util.Objects;
 
 public class renderHelperFunction {
     public static void renderHud(DrawContext drawContext) {
@@ -14,6 +19,12 @@ public class renderHelperFunction {
 
         assert player != null;
         drawWatermark(drawContext, textRenderer, player.age);
+
+        drawModulesList(drawContext, textRenderer, player.age);
+
+        if(Objects.requireNonNull(ModuleManager.getModule(FPSHudModule.class)).getEnabledStatus()) {
+            drawFPS(drawContext, textRenderer, player.age);
+        }
     }
 
     public static void drawWatermark(DrawContext drawContext, TextRenderer textRenderer, int age) {
@@ -24,9 +35,10 @@ public class renderHelperFunction {
         var matrices = drawContext.getMatrices().peek().getPositionMatrix();
         matrices.scale(scale);
 
+        assert MinecraftClient.getInstance().player != null;
         textRenderer.draw(
                 chatHelperFunction.getPrefix()+" [v"+clientVersion+"]",
-                5, 5,
+                5,5,
                 textColor, true,
                 matrices,
                 drawContext.getVertexConsumers(),
@@ -36,5 +48,71 @@ public class renderHelperFunction {
 
         matrices.scale(1f / scale);
         drawContext.draw();
+    }
+
+    public static void drawFPS(DrawContext drawContext, TextRenderer textRenderer, int age) {
+        var scale = 1f;
+        int textColor = 0xAAAAAA;
+
+        var matrices = drawContext.getMatrices().peek().getPositionMatrix();
+        matrices.scale(scale);
+
+        assert MinecraftClient.getInstance().player != null;
+        textRenderer.draw(
+                MinecraftClient.getInstance().getCurrentFps() + " [FPS]",
+                5,20,
+                textColor, true,
+                matrices,
+                drawContext.getVertexConsumers(),
+                TextRenderer.TextLayerType.NORMAL,
+                0, 0xF000F0
+        );
+
+        matrices.scale(1f / scale);
+        drawContext.draw();
+    }
+
+    public static void drawModulesList(DrawContext drawContext, TextRenderer textRenderer, int age) {
+        int yOffset = 5;
+
+        var scale = 1f;
+        int textColor = 0xFFFFFFFF;
+
+        var matrices = drawContext.getMatrices().peek().getPositionMatrix();
+        matrices.scale(scale);
+
+        int screenWidth = MinecraftClient.getInstance().getWindow().getScaledWidth();
+
+        for(ModuleImplementation module: ModuleManager.getModules()) {
+            if(Objects.requireNonNull(ModuleManager.getModule(module.getClass())).getEnabledStatus()) {
+                textRenderer.draw(
+                        annotationHelperFunction.getName(module.getClass()),
+                        screenWidth - MinecraftClient.getInstance().textRenderer.getWidth(annotationHelperFunction.getName(module.getClass())) - 5, yOffset,
+                        textColor, true,
+                        matrices,
+                        drawContext.getVertexConsumers(),
+                        TextRenderer.TextLayerType.NORMAL,
+                        0, 0xF000F0
+                );
+
+                matrices.scale(1f / scale);
+                drawContext.draw();
+
+            }
+            yOffset += 10;
+        }
+    }
+
+    public static void renderScreenOverlay(DrawContext drawContext) {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        drawContext.drawText(
+                client.textRenderer,
+                "Loaded Nexsus",
+                5,
+                5,
+                0xFFFFFFFF,
+                true
+        );
     }
 }
