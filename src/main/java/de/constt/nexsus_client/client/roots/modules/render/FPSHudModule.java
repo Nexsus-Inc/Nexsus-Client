d06908936fe33344b62e9549f01a5e90c127fb8c
constt <175602385+constttdev@users.noreply.github.com>
2025-12-17 21:03:07 +0100

    feat: fps hud module

---
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/render/FPSHudModule.java
@@ -0,0 +1,14 @@
+package de.constt.nexsus_client.client.roots.modules.render;
+
+
+import de.constt.nexsus_client.client.annotations.InfoAnnotation;
+import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
+import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
+
+@InfoAnnotation(
+        name = "FPS (Hud)",
+        description = "Shows your FPS in the HUD",
+        category = CategoryImplementation.Categories.RENDER
+)
+public class FPSHudModule extends ModuleImplementation {
+}
