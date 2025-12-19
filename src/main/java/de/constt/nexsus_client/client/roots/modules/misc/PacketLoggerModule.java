1c5cbb26618d9fee4a2761af5681ab30ba028450
constt <175602385+constttdev@users.noreply.github.com>
2025-12-20 00:14:52 +0100

    feat: annotation rename

--- src/main/java/de/constt/nexsus_client/client/roots/modules/misc/PacketLoggerModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/misc/PacketLoggerModule.java
@@ -1,15 +1,16 @@
 package de.constt.nexsus_client.client.roots.modules.misc;
 
-import de.constt.nexsus_client.client.annotations.InfoAnnotation;
-import de.constt.nexsus_client.client.helperFunctions.chatHelperFunction;
+import de.constt.nexsus_client.client.annotations.ModuleInfoAnnotation;
+import de.constt.nexsus_client.client.helperFunctions.ChatHelperFunction;
 import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 import net.minecraft.network.packet.Packet;
 
-@InfoAnnotation(
-        name = "Packet-Logger",
+@ModuleInfoAnnotation(
+        name = "Packet Logger",
         description = "Logs packets in chat",
-        category = CategoryImplementation.Categories.MISC
+        category = CategoryImplementation.Categories.MISC,
+        internalModuleName = "packetlogger"
 )
 public class PacketLoggerModule extends ModuleImplementation {
 
@@ -18,7 +19,7 @@ public class PacketLoggerModule extends ModuleImplementation {
         if (!enabled) return false;
 
         String packetInfo = packet.getClass().getSimpleName();
-        chatHelperFunction.sendCSMessageNeutral("Packet sent/received: " + packetInfo);
+        ChatHelperFunction.sendCSMessageNeutral("Packet sent/received: " + packetInfo);
 
         return false;
     }
