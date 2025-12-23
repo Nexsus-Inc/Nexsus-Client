d0c0742d69a817c077defc70ebe1e3db8f271f97
constt <175602385+constttdev@users.noreply.github.com>
2025-12-23 16:14:45 +0100

    feat: new import

--- src/main/java/de/constt/nexsus_client/client/roots/modules/misc/DebuggerModule.java
+++ src/main/java/de/constt/nexsus_client/client/roots/modules/misc/DebuggerModule.java
@@ -4,6 +4,8 @@ import de.constt.nexsus_client.client.annotations.ModuleInfoAnnotation;
 import de.constt.nexsus_client.client.roots.implementations.CategoryImplementation;
 import de.constt.nexsus_client.client.roots.implementations.ModuleImplementation;
 
+import java.awt.*;
+
 @ModuleInfoAnnotation(
         name = "Debugger",
         description = "Debug modules and the client",
